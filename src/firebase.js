d6386338a029a99e114ac82e15ef40d448d1c3c9
Albert Garcia Jr <albertrgarciajr@gmail.com>
2025-10-05 11:06:34 +0800

    4th Commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // <-- This line was likely missing or incorrect
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"; // <-- This line was likely the issue
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
